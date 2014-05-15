b0d48c876de2a81cf0ce65d7fa0cd2f96a94881e
xiexin <xiexin@chukong-inc.com>
2014-05-15 14:08:35 +0800

    1 修改cocos2d默认实现,实现处理.

--- cocos2dx/platform/mac/EAGLView.mm
+++ cocos2dx/platform/mac/EAGLView.mm
@@ -53,72 +53,72 @@ static EAGLView *view;
 	return view;
 }
 
-- (id) initWithFrame:(NSRect)frameRect
-{
-	self = [self initWithFrame:frameRect shareContext:nil];
-	return self;
-}
-
-- (id) initWithFrame:(NSRect)frameRect shareContext:(NSOpenGLContext*)context
-{
-    NSOpenGLPixelFormatAttribute attribs[] =
-    {
-//		NSOpenGLPFAAccelerated,
-//		NSOpenGLPFANoRecovery,
-		NSOpenGLPFADoubleBuffer,
-		NSOpenGLPFADepthSize, 24,
-		
-		0
-    };
-	
-	NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
-	
-	if (!pixelFormat)
-		NSLog(@"No OpenGL pixel format");
-	
-	if( (self = [super initWithFrame:frameRect pixelFormat:[pixelFormat autorelease]]) ) {
-		
-		if( context )
-			[self setOpenGLContext:context];
-
-		// event delegate
-		eventDelegate_ = [CCEventDispatcher sharedDispatcher];
-	}
-    
-    cocos2d::CCEGLView::sharedOpenGLView()->setFrameSize(frameRect.size.width, frameRect.size.height);
-    
-    frameZoomFactor_ = 1.0f;
-	
-	view = self;
-	return self;
-}
-
-- (id) initWithFrame:(NSRect)frameRect pixelFormat:(NSOpenGLPixelFormat *)format{
-    // event delegate
-    eventDelegate_ = [CCEventDispatcher sharedDispatcher];
-    
-    cocos2d::CCEGLView::sharedOpenGLView()->setFrameSize(frameRect.size.width, frameRect.size.height);
-    
-    frameZoomFactor_ = 1.0f;
-	
-	view = self;
-    
-    [super initWithFrame:frameRect pixelFormat:format];
-
-    return self;
-}
+//- (id) initWithFrame:(NSRect)frameRect
+//{
+//	self = [self initWithFrame:frameRect shareContext:nil];
+//	return self;
+//}
+//
+//- (id) initWithFrame:(NSRect)frameRect shareContext:(NSOpenGLContext*)context
+//{
+//    NSOpenGLPixelFormatAttribute attribs[] =
+//    {
+////		NSOpenGLPFAAccelerated,
+////		NSOpenGLPFANoRecovery,
+//		NSOpenGLPFADoubleBuffer,
+//		NSOpenGLPFADepthSize, 24,
+//		
+//		0
+//    };
+//	
+//	NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
+//	
+////	if (!pixelFormat)
+////		NSLog(@"No OpenGL pixel format");
+////	
+////	if( (self = [super initWithFrame:frameRect pixelFormat:[pixelFormat autorelease]]) ) {
+////		
+////		if( context )
+////			[self setOpenGLContext:context];
+////
+////		// event delegate
+////		eventDelegate_ = [CCEventDispatcher sharedDispatcher];
+////	}
+//    
+//    cocos2d::CCEGLView::sharedOpenGLView()->setFrameSize(frameRect.size.width, frameRect.size.height);
+//    
+//    frameZoomFactor_ = 1.0f;
+//	
+//	view = self;
+//	return self;
+//}
+//
+//- (id) initWithFrame:(NSRect)frameRect pixelFormat:(NSOpenGLPixelFormat *)format{
+//    // event delegate
+//    eventDelegate_ = [CCEventDispatcher sharedDispatcher];
+//    
+//    cocos2d::CCEGLView::sharedOpenGLView()->setFrameSize(frameRect.size.width, frameRect.size.height);
+//    
+//    frameZoomFactor_ = 1.0f;
+//	
+//	view = self;
+//    
+//    //[super initWithFrame:frameRect pixelFormat:format];
+//
+//    return self;
+//}
 
 - (void) update
 {
 	// XXX: Should I do something here ?
-	[super update];
+	//[super update];
 }
 
 - (void) prepareOpenGL
 {
 	// XXX: Initialize OpenGL context
 
-	[super prepareOpenGL];
+	//[super prepareOpenGL];
 	
 	// Make this openGL context current to the thread
 	// (i.e. all openGL on this thread calls will go to this context)
@@ -126,7 +126,7 @@ static EAGLView *view;
 	
 	// Synchronize buffer swaps with vertical refresh rate
 	GLint swapInt = 1;
-	[[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];	
+	[[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
 
 //	GLint order = -1;
 //	[[self openGLContext] setValues:&order forParameter:NSOpenGLCPSurfaceOrder];
@@ -141,8 +141,8 @@ static EAGLView *view;
 {
     frameZoomFactor_ = frameZoomFactor;
     
-    NSRect winRect = [[self window] frame];
-    NSRect viewRect = [self frame];
+    NSRect winRect = [[drawView window] frame];
+    NSRect viewRect = [drawView frame];
     
     // compute the margin width and margin height
     float diffX = winRect.size.width - viewRect.size.width;
@@ -157,7 +157,7 @@ static EAGLView *view;
     float originX = (screenRect.size.width - newWindowWidth) / 2;
     float originY = (screenRect.size.height - newWindowHeight) / 2;
     
-    [[self window] setFrame:NSMakeRect(originX, originY, newWindowWidth, newWindowHeight) display:true];
+    [[drawView window] setFrame:NSMakeRect(originX, originY, newWindowWidth, newWindowHeight) display:true];
 }
 
 - (void) reshape
@@ -193,7 +193,7 @@ static EAGLView *view;
 
 -(void) unlockOpenGLContext
 {
-	NSOpenGLContext *glContext = [self openGLContext];
+	NSOpenGLContext *glContext =[self openGLContext];
 	NSAssert( glContext, @"FATAL: could not get openGL context");
 
 	CGLUnlockContext((CGLContextObj)[glContext CGLContextObj]);
@@ -202,18 +202,18 @@ static EAGLView *view;
 - (void) dealloc
 {
 	CCLOGINFO(@"cocos2d: deallocing EAGLView %@", self);
-	[super dealloc];
+    [super dealloc];
 }
 	
 -(int) getWidth
 {
-	NSSize bound = [self bounds].size;
+	NSSize bound = [drawView bounds].size;
 	return bound.width;
 }
 
 -(int) getHeight
 {
-	NSSize bound = [self bounds].size;
+	NSSize bound = [drawView bounds].size;
 	return bound.height;
 }
 
@@ -229,77 +229,77 @@ static EAGLView *view;
 	// Mac OS X 10.6 and later offer a simplified mechanism to create full-screen contexts
 #if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_5
 
-    if (isFullScreen_ == fullscreen)
-		return;
-
-	EAGLView *openGLview = [[self class] sharedEGLView];
-
-    if( fullscreen ) {
-        originalWinRect_ = [openGLview frame];
-
-        // Cache normal window and superview of openGLView
-        if(!windowGLView_)
-            windowGLView_ = [[openGLview window] retain];
-
-        [superViewGLView_ release];
-        superViewGLView_ = [[openGLview superview] retain];
-
-
-        // Get screen size
-        NSRect displayRect = [[NSScreen mainScreen] frame];
-
-        // Create a screen-sized window on the display you want to take over
-        fullScreenWindow_ = [[CCWindow alloc] initWithFrame:displayRect fullscreen:YES];
-
-        // Remove glView from window
-        [openGLview removeFromSuperview];
-
-        // Set new frame
-        [openGLview setFrame:displayRect];
-
-        // Attach glView to fullscreen window
-        [fullScreenWindow_ setContentView:openGLview];
-
-        // Show the fullscreen window
-        [fullScreenWindow_ makeKeyAndOrderFront:self];
-		[fullScreenWindow_ makeMainWindow];
-		//[fullScreenWindow_ setNextResponder:superViewGLView_];
-
-    } else {
-
-        // Remove glView from fullscreen window
-        [openGLview removeFromSuperview];
-
-        // Release fullscreen window
-        [fullScreenWindow_ release];
-        fullScreenWindow_ = nil;
-
-        // Attach glView to superview
-        [superViewGLView_ addSubview:openGLview];
-
-        // Set new frame
-        [openGLview setFrame:originalWinRect_];
-
-        // Show the window
-        [windowGLView_ makeKeyAndOrderFront:self];
-		[windowGLView_ makeMainWindow];
-    }
-	
-	// issue #1189
-	[windowGLView_ makeFirstResponder:openGLview];
-
-    isFullScreen_ = fullscreen;
-
-    //[openGLview retain]; // Retain +1
-
-	// is this necessary?
-    // re-configure glView
-	//cocos2d::CCDirector *director = cocos2d::CCDirector::sharedDirector();
-	//director->setOpenGLView(openGLview); //[self setView:openGLview];
-
-    //[openGLview release]; // Retain -1
-
-    [openGLview setNeedsDisplay:YES];
+//    if (isFullScreen_ == fullscreen)
+//		return;
+//
+//	EAGLView *openGLview = [[self class] sharedEGLView];
+//
+//    if( fullscreen ) {
+//        originalWinRect_ = [openGLview frame];
+//
+//        // Cache normal window and superview of openGLView
+//        if(!windowGLView_)
+//            windowGLView_ = [[openGLview window] retain];
+//
+//        [superViewGLView_ release];
+//        superViewGLView_ = [[openGLview superview] retain];
+//
+//
+//        // Get screen size
+//        NSRect displayRect = [[NSScreen mainScreen] frame];
+//
+//        // Create a screen-sized window on the display you want to take over
+//        fullScreenWindow_ = [[CCWindow alloc] initWithFrame:displayRect fullscreen:YES];
+//
+//        // Remove glView from window
+//        [openGLview removeFromSuperview];
+//
+//        // Set new frame
+//        [openGLview setFrame:displayRect];
+//
+//        // Attach glView to fullscreen window
+//        [fullScreenWindow_ setContentView:openGLview];
+//
+//        // Show the fullscreen window
+//        [fullScreenWindow_ makeKeyAndOrderFront:self];
+//		[fullScreenWindow_ makeMainWindow];
+//		//[fullScreenWindow_ setNextResponder:superViewGLView_];
+//
+//    } else {
+//
+//        // Remove glView from fullscreen window
+//        [openGLview removeFromSuperview];
+//
+//        // Release fullscreen window
+//        [fullScreenWindow_ release];
+//        fullScreenWindow_ = nil;
+//
+//        // Attach glView to superview
+//        [superViewGLView_ addSubview:openGLview];
+//
+//        // Set new frame
+//        [openGLview setFrame:originalWinRect_];
+//
+//        // Show the window
+//        [windowGLView_ makeKeyAndOrderFront:self];
+//		[windowGLView_ makeMainWindow];
+//    }
+//	
+//	// issue #1189
+//	[windowGLView_ makeFirstResponder:openGLview];
+//
+//    isFullScreen_ = fullscreen;
+//
+//    //[openGLview retain]; // Retain +1
+//
+//	// is this necessary?
+//    // re-configure glView
+//	//cocos2d::CCDirector *director = cocos2d::CCDirector::sharedDirector();
+//	//director->setOpenGLView(openGLview); //[self setView:openGLview];
+//
+//    //[openGLview release]; // Retain -1
+//
+//    [openGLview setNeedsDisplay:YES];
 #else
 #error Full screen is not supported for Mac OS 10.5 or older yet
 #error If you don't want FullScreen support, you can safely remove these 2 lines
@@ -317,173 +317,227 @@ static EAGLView *view;
 	   waitUntilDone:NO];
 #endif
 
+-(void) setDrawView:(NSView*)view
+{
+    drawView=view;
+}
+
+- (id) initWithFrame:(NSRect)frameRect drawView:(NSView*) v
+{
+    self=[super init];
+	if (!v)
+	{
+		return NULL;
+	}
+    drawView = v;
+    
+	NSOpenGLPixelFormatAttribute attribs[] = {
+		NSOpenGLPFAColorSize, 24,
+		NSOpenGLPFAAlphaSize, 8,
+		NSOpenGLPFADepthSize, 1,
+		NSOpenGLPFADoubleBuffer,
+		0
+	};
+    
+	NSOpenGLPixelFormat* pixelFormat= [[NSOpenGLPixelFormat alloc] initWithAttributes:attribs];
+	NSOpenGLContext* openGLContext = [[NSOpenGLContext  alloc] initWithFormat:pixelFormat shareContext:nil];
+    
+    if(openGLContext)
+    {
+        [self setOpenGLContext:openGLContext];
+        [self setDrawView:drawView];
+        [openGLContext  makeCurrentContext];
+        [drawView setNeedsDisplay:YES];
+    }
+    
+    cocos2d::CCEGLView::sharedOpenGLView()->setFrameSize(frameRect.size.width, frameRect.size.height);
+    
+    frameZoomFactor_ = 1.0f;
+	view = self;
+    drawView=self;
+	return self;
+}
+
+-(void)ensureDrawView
+{
+    NSView* glView=[[self openGLContext] view];
+    if (glView != self)
+		[[self openGLContext]  setView:self];
+}
+
+-(NSView*) getDrawView
+{
+    return drawView;
+}
+
+
 #pragma mark EAGLView - Mouse events
 
-- (void)mouseDown:(NSEvent *)theEvent
-{
-	NSPoint event_location = [theEvent locationInWindow];
-	NSPoint local_point = [self convertPoint:event_location fromView:nil];
-	
-	float x = local_point.x;
-	float y = [self getHeight] - local_point.y;
-	
-    int ids[1] = {0};
-    float xs[1] = {0.0f};
-    float ys[1] = {0.0f};
-    
-	ids[0] = [theEvent eventNumber];
-	xs[0] = x / frameZoomFactor_;
-	ys[0] = y / frameZoomFactor_;
-
-	cocos2d::CCDirector::sharedDirector()->getOpenGLView()->handleTouchesBegin(1, ids, xs, ys);
-}
-
-- (void)mouseMoved:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-}
-
-- (void)mouseDragged:(NSEvent *)theEvent
-{
-	NSPoint event_location = [theEvent locationInWindow];
-	NSPoint local_point = [self convertPoint:event_location fromView:nil];
-	
-	float x = local_point.x;
-	float y = [self getHeight] - local_point.y;
-
-    int ids[1] = {0};
-    float xs[1] = {0.0f};
-    float ys[1] = {0.0f};
-    
-	ids[0] = [theEvent eventNumber];
-	xs[0] = x / frameZoomFactor_;
-	ys[0] = y / frameZoomFactor_;
-
-	cocos2d::CCDirector::sharedDirector()->getOpenGLView()->handleTouchesMove(1, ids, xs, ys);
-}
-
-- (void)mouseUp:(NSEvent *)theEvent
-{
-	NSPoint event_location = [theEvent locationInWindow];
-	NSPoint local_point = [self convertPoint:event_location fromView:nil];
-	
-	float x = local_point.x;
-	float y = [self getHeight] - local_point.y;
-
-    int ids[1] = {0};
-    float xs[1] = {0.0f};
-    float ys[1] = {0.0f};
-    
-	ids[0] = [theEvent eventNumber];
-	xs[0] = x / frameZoomFactor_;
-	ys[0] = y / frameZoomFactor_;
-
-	cocos2d::CCDirector::sharedDirector()->getOpenGLView()->handleTouchesEnd(1, ids, xs, ys);
-}
-
-- (void)rightMouseDown:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-
-	// pass the event along to the next responder (like your NSWindow subclass)
-	[super rightMouseDown:theEvent];
-}
-
-- (void)rightMouseDragged:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super rightMouseDragged:theEvent];
-}
-
-- (void)rightMouseUp:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super rightMouseUp:theEvent];
-}
-
-- (void)otherMouseDown:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super otherMouseDown:theEvent];
-}
-
-- (void)otherMouseDragged:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super otherMouseDragged:theEvent];
-}
-
-- (void)otherMouseUp:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super otherMouseUp:theEvent];
-}
-
-- (void)mouseEntered:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super mouseEntered:theEvent];
-}
-
-- (void)mouseExited:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super mouseExited:theEvent];
-}
-
--(void) scrollWheel:(NSEvent *)theEvent {
-	DISPATCH_EVENT(theEvent, _cmd);
-	[super scrollWheel:theEvent];
-}
-
-#pragma mark EAGLView - Key events
-
--(BOOL) becomeFirstResponder
-{
-	return YES;
-}
-
--(BOOL) acceptsFirstResponder
-{
-	return YES;
-}
-
--(BOOL) resignFirstResponder
-{
-	return YES;
-}
-
-- (void)keyDown:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-	
-	// pass the event along to the next responder (like your NSWindow subclass)
-	[super keyDown:theEvent];
-}
-
-- (void)keyUp:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-
-	// pass the event along to the next responder (like your NSWindow subclass)
-	[super keyUp:theEvent];
-}
-
-- (void)flagsChanged:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-}
-
-#pragma mark EAGLView - Touch events
-- (void)touchesBeganWithEvent:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-}
-
-- (void)touchesMovedWithEvent:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-}
-
-- (void)touchesEndedWithEvent:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-}
-
-- (void)touchesCancelledWithEvent:(NSEvent *)theEvent
-{
-	DISPATCH_EVENT(theEvent, _cmd);
-}
+//- (void)mouseDown:(NSEvent *)theEvent
+//{
+//	NSPoint event_location = [theEvent locationInWindow];
+//	NSPoint local_point = [self convertPoint:event_location fromView:nil];
+//	
+//	float x = local_point.x;
+//	float y = [self getHeight] - local_point.y;
+//	
+//    int ids[1] = {0};
+//    float xs[1] = {0.0f};
+//    float ys[1] = {0.0f};
+//    
+//	ids[0] = [theEvent eventNumber];
+//	xs[0] = x / frameZoomFactor_;
+//	ys[0] = y / frameZoomFactor_;
+//
+//	cocos2d::CCDirector::sharedDirector()->getOpenGLView()->handleTouchesBegin(1, ids, xs, ys);
+//}
+//
+//- (void)mouseMoved:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//}
+//
+//- (void)mouseDragged:(NSEvent *)theEvent
+//{
+//	NSPoint event_location = [theEvent locationInWindow];
+//	NSPoint local_point = [self convertPoint:event_location fromView:nil];
+//	
+//	float x = local_point.x;
+//	float y = [self getHeight] - local_point.y;
+//
+//    int ids[1] = {0};
+//    float xs[1] = {0.0f};
+//    float ys[1] = {0.0f};
+//    
+//	ids[0] = [theEvent eventNumber];
+//	xs[0] = x / frameZoomFactor_;
+//	ys[0] = y / frameZoomFactor_;
+//
+//	cocos2d::CCDirector::sharedDirector()->getOpenGLView()->handleTouchesMove(1, ids, xs, ys);
+//}
+//
+//- (void)mouseUp:(NSEvent *)theEvent
+//{
+//	NSPoint event_location = [theEvent locationInWindow];
+//	NSPoint local_point = [self convertPoint:event_location fromView:nil];
+//	
+//	float x = local_point.x;
+//	float y = [self getHeight] - local_point.y;
+//
+//    int ids[1] = {0};
+//    float xs[1] = {0.0f};
+//    float ys[1] = {0.0f};
+//    
+//	ids[0] = [theEvent eventNumber];
+//	xs[0] = x / frameZoomFactor_;
+//	ys[0] = y / frameZoomFactor_;
+//
+//	cocos2d::CCDirector::sharedDirector()->getOpenGLView()->handleTouchesEnd(1, ids, xs, ys);
+//}
+//
+//- (void)rightMouseDown:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//
+//	// pass the event along to the next responder (like your NSWindow subclass)
+//	[super rightMouseDown:theEvent];
+//}
+//
+//- (void)rightMouseDragged:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super rightMouseDragged:theEvent];
+//}
+//
+//- (void)rightMouseUp:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super rightMouseUp:theEvent];
+//}
+//
+//- (void)otherMouseDown:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super otherMouseDown:theEvent];
+//}
+//
+//- (void)otherMouseDragged:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super otherMouseDragged:theEvent];
+//}
+//
+//- (void)otherMouseUp:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super otherMouseUp:theEvent];
+//}
+//
+//- (void)mouseEntered:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super mouseEntered:theEvent];
+//}
+//
+//- (void)mouseExited:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super mouseExited:theEvent];
+//}
+//
+//-(void) scrollWheel:(NSEvent *)theEvent {
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	[super scrollWheel:theEvent];
+//}
+//
+//#pragma mark EAGLView - Key events
+//
+//-(BOOL) becomeFirstResponder
+//{
+//	return YES;
+//}
+//
+//-(BOOL) acceptsFirstResponder
+//{
+//	return YES;
+//}
+//
+//-(BOOL) resignFirstResponder
+//{
+//	return YES;
+//}
+//
+//- (void)keyDown:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//	
+//	// pass the event along to the next responder (like your NSWindow subclass)
+//	[super keyDown:theEvent];
+//}
+//
+//- (void)keyUp:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//
+//	// pass the event along to the next responder (like your NSWindow subclass)
+//	[super keyUp:theEvent];
+//}
+//
+//- (void)flagsChanged:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//}
+//
+//#pragma mark EAGLView - Touch events
+//- (void)touchesBeganWithEvent:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//}
+//
+//- (void)touchesMovedWithEvent:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//}
+//
+//- (void)touchesEndedWithEvent:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//}
+//
+//- (void)touchesCancelledWithEvent:(NSEvent *)theEvent
+//{
+//	DISPATCH_EVENT(theEvent, _cmd);
+//}
 @end
